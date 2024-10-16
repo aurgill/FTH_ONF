5dfbed87847e5a4ff469d878e1d2128cf86a63fc
aurgill <aurgil@yahoo.fr>
2024-10-16 03:49:10 +0200

    plot des dbh

--- Script.R
+++ Script.R
@@ -2,8 +2,22 @@
 ##Aurèlia BALAYER - Juliette XXX - Aurélien GILLARD
 rm(list=ls()) #nettoyage des variables
 library(readr)
+library(entropart) #pour autoplot
+library(tidyverse)
 hp=read_csv(file = "Inv_HP.csv")
 pst=read_csv(file = "Inv_Piste.csv")
 
-plot(hp$dbh)
-plot(pst$dbh)
+df = as.data.frame(table(as.vector(hp$dbh)))
+dbhHP <- df$Freq
+names(dbhHP) <- df$Var1
+# Vecteur pour le traitement par autoplot
+dbhHP %>% as.AbdVector -> dbhHP
+autoplot(dbhHP, Distribution = "lnorm") #utilise ggplot automatiquement.
+
+df = as.data.frame(table(as.vector(pst$dbh)))
+dbhPst <- df$Freq
+names(dbhPst) <- df$Var1
+# Vecteur pour le traitement par autoplot
+dbhPst %>% as.AbdVector -> dbhPst
+autoplot(dbhPst, Distribution = "lnorm") #utilise ggplot automatiquement.
+
